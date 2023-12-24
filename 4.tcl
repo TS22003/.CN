2ba77f5d5cc73aa86dc81d38ba2268482df338d4
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-12-24 15:26:45 +0530

    Update 4.tcl

--- 4.tcl
+++ 4.tcl
@@ -1,22 +1,11 @@
-#Create a ns simulator
 set ns [new Simulator]
-
-#Setup topography object
 set topo [new Topography]
 $topo load_flatgrid 1500 1500
-
-#Open the NS trace file
 set tracefile [open p6.tr w]
 $ns trace-all $tracefile
-
-#Open the NAM trace file
 set namfile [open p6.nam w]
 $ns namtrace-all $namfile
 $ns namtrace-all-wireless $namfile 1500 1500
-
-#===================================
-# Mobile node parameter setup
-#===================================
 $ns node-config -adhocRouting DSDV \
 	-llType LL \
 	-macType Mac/802_11 \
@@ -29,12 +18,7 @@ $ns node-config -adhocRouting DSDV \
 	-topoInstance $topo \
 	-agentTrace ON \
 	-routerTrace ON
-
-#===================================
-# Nodes Definition
-#===================================
 create-god 6
-#Create 6 nodes
 set n0 [$ns node]
 $n0 set X_ 630
 $n0 set Y_ 501
@@ -65,43 +49,24 @@ $n5 set X_ 964
 $n5 set Y_ 177
 $n5 set Z_ 0.0
 $ns initial_node_pos $n5 20
-
-#===================================
-# Agents Definition
-#===================================
-#Setup a UDP connection
 set udp0 [new Agent/UDP]
 $ns attach-agent $n0 $udp0
 set null1 [new Agent/Null]
 $ns attach-agent $n4 $null1
 $ns connect $udp0 $null1
 $udp0 set packetSize_ 1500
-
-#Setup a TCP connection
 set tcp0 [new Agent/TCP]
 $ns attach-agent $n3 $tcp0
 set sink1 [new Agent/TCPSink]
 $ns attach-agent $n5 $sink1
 $ns connect $tcp0 $sink1
-
-#===================================
-# Applications Definition
-#===================================
-#Setup a CBR Application over UDP connection
 set cbr0 [new Application/Traffic/CBR]
 $cbr0 attach-agent $udp0
 $cbr0 set packetSize_ 1000
 $cbr0 set rate_ 1.0Mb
 $cbr0 set random_ null
-
-#Setup a FTP Application over TCP connection
 set ftp0 [new Application/FTP]
 $ftp0 attach-agent $tcp0
-
-#===================================
-# Termination
-#===================================
-#Define a 'finish' procedure
 proc finish {} {
 global ns tracefile namfile
 $ns flush-trace
@@ -112,7 +77,6 @@ exec echo "Number of packets dropped is : " &
 exec grep -c "^D" p6.tr &
 exit 0
 }
-
 $ns at 1.0 "$cbr0 start"
 $ns at 2.0 "$ftp0 start"
 $ns at 180.0 "$ftp0 stop"
@@ -122,12 +86,8 @@ $ns at 70 "$n4 setdest 100 60 20"
 $ns at 100 "$n4 setdest 700 300 20"
 $ns at 150 "$n4 setdest 900 200 20"
 $ns run
-
-
-//===================================
-// AWK file (filename.awk)
-//===================================
-
+#create a file called filename.awk 
+#cmd:awk -f process_output.awk output_file
 BEGIN{
 count1=0
 count2=0
