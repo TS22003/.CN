2c94e64c10807e83d61213eecc384e64212ab234
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-12-25 22:20:10 +0530

    Update token_bucket.java

--- token_bucket.java
+++ token_bucket.java
@@ -1,3 +1,4 @@
+//Token bucket
 import java.util.Scanner;
 public class Tokenbucket {
     public static void main(String[] args) {
@@ -34,7 +35,40 @@ public class Tokenbucket {
                 System.out.println("Request denied, not enough tokens: " + tokens);
             }
         }
-
+        scanner.close();
+    }
+}
+//Leaky-bucket
+import java.util.Scanner;
+public class LeakyBucket {
+    public static void main(String[] args) {
+        int noOfQueries;
+        int bucketSize;
+        int inputPacketSize;
+        int outputPacketSize;
+        int storedBufferSize = 0;
+        int sizeLeft;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER THE BUCKET SIZE");
+        bucketSize = scanner.nextInt();
+        System.out.println("ENTER THE NUMBER OF PACKETS");
+        noOfQueries = scanner.nextInt();
+        System.out.println("ENTER THE OUTPUT RATE");
+        outputPacketSize = scanner.nextInt();
+        for (int i = 0; i < noOfQueries; i++) {
+            System.out.print("ENTER THE SIZE OF THE PACKET :");
+            inputPacketSize = scanner.nextInt();
+            sizeLeft = bucketSize - storedBufferSize;
+            if (inputPacketSize <= sizeLeft) {
+                storedBufferSize += inputPacketSize;
+                System.out.println("Number of Packets added into Bucket" + inputPacketSize);
+            } else {
+                System.out.println("Number of Packets Dropped " + (storedBufferSize + inputPacketSize - bucketSize));
+                storedBufferSize = bucketSize;
+            }
+            /* System.out.println("Stored Buffer Size: " + storedBufferSize); */
+            storedBufferSize -= outputPacketSize;
+        }
         scanner.close();
     }
 }
