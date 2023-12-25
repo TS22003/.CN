d6e7220376e1954166eaee483abbee84ed26270c
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-12-25 22:18:37 +0530

    Update token_bucket.java

--- token_bucket.java
+++ token_bucket.java
@@ -1,427 +1,48 @@
-/*
- * Copyright 2012-2014 Brandon Beck
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.isomorphism.util;
+import java.util.Scanner;
 
-import java.util.concurrent.TimeUnit;
+public class Tokenbucket {
+    public static void main(String[] args) {
+        int tokens = 0; 
+        int rate = 10;
+        int capacity;
 
-/**
- * A token bucket is used for rate limiting access to a portion of code.
- *
- * @see <a href="http://en.wikipedia.org/wiki/Token_bucket">Token Bucket on Wikipedia</a>
- * @see <a href="http://en.wikipedia.org/wiki/Leaky_bucket">Leaky Bucket on Wikipedia</a>
- */
-public interface TokenBucket
-{
-  /**
-   * Returns the capacity of this token bucket.  This is the maximum number of tokens that the bucket can hold at
-   * any one time.
-   *
-   * @return The capacity of the bucket.
-   */
-  long getCapacity();
+        int[] request = new int[100];
 
-  /**
-   * Returns the current number of tokens in the bucket.  If the bucket is empty then this method will return 0.
-   *
-   * @return The current number of tokens in the bucket.
-   */
-  long getNumTokens();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER THE BUCKET SIZE");
+        capacity = scanner.nextInt();
 
-  /**
-   * Returns the amount of time in the specified time unit until the next group of tokens can be added to the token
-   * bucket.
-   *
-   * @see org.isomorphism.util.TokenBucket.RefillStrategy#getDurationUntilNextRefill(java.util.concurrent.TimeUnit)
-   * @param unit The time unit to express the return value in.
-   * @return The amount of time until the next group of tokens can be added to the token bucket.
-   */
-  long getDurationUntilNextRefill(TimeUnit unit) throws UnsupportedOperationException;
+        System.out.print("Enter number of requests: ");
+        int n = scanner.nextInt();
 
-  /**
-   * Attempt to consume a single token from the bucket.  If it was consumed then {@code true} is returned, otherwise
-   * {@code false} is returned.
-   *
-   * @return {@code true} if a token was consumed, {@code false} otherwise.
-   */
-  boolean tryConsume();
+        System.out.print("Enter no. of packets per request: ");
+        for (int i = 0; i < n; i++) {
+            request[i] = scanner.nextInt();
+        }
 
-  /**
-   * Attempt to consume a specified number of tokens from the bucket.  If the tokens were consumed then {@code true}
-   * is returned, otherwise {@code false} is returned.
-   *
-   * @param numTokens The number of tokens to consume from the bucket, must be a positive number.
-   * @return {@code true} if the tokens were consumed, {@code false} otherwise.
-   */
-  boolean tryConsume(long numTokens);
+        for (int i = 0; i < n; i++) {
+            // add tokens to the bucket at a fixed rate
+            tokens = Math.min(tokens + rate, capacity);
 
-  /**
-   * Consume a single token from the bucket.  If no token is currently available then this method will block until a
-   * token becomes available.
-   */
-  void consume();
+            /*
+             * try IF U WANT U CAN ADD JUST TO SHOW PACKET MOVING TIME
+             * {
+             * // wait for 1 second
+             * Thread.sleep(1000);
+             * } catch (InterruptedException e) {
+             * e.printStackTrace();
+             * }
+             */
+            System.out.println("Number of packets" + request[i]);
+            if (tokens >= request[i]) {
+                // remove the requested tokens from the bucket
+                tokens -= request[i];
+                System.out.println("Request granted, tokens remaining: " + tokens);
+            } else {
+                System.out.println("Request denied, not enough tokens: " + tokens);
+            }
+        }
 
-  /**
-   * Consumes multiple tokens from the bucket.  If enough tokens are not currently available then this method will block
-   * until
-   *
-   * @param numTokens The number of tokens to consume from teh bucket, must be a positive number.
-   */
-  void consume(long numTokens);
-
-  /**
-   * Refills the bucket with the specified number of tokens.  If the bucket is currently full or near capacity then
-   * fewer than {@code numTokens} may be added.
-   *
-   * @param numTokens The number of tokens to add to the bucket.
-   */
-  void refill(long numTokens);
-
-  /** Encapsulation of a refilling strategy for a token bucket. */
-  static interface RefillStrategy
-  {
-    /**
-     * Returns the number of tokens to add to the token bucket.
-     *
-     * @return The number of tokens to add to the token bucket.
-     */
-    long refill();
-
-    /**
-     * Returns the amount of time in the specified time unit until the next group of tokens can be added to the token
-     * bucket.  Please note, depending on the {@code SleepStrategy} used by the token bucket, tokens may not actually
-     * be added until much after the returned duration.  If for some reason the implementation of
-     * {@code RefillStrategy} doesn't support knowing when the next batch of tokens will be added, then an
-     * {@code UnsupportedOperationException} may be thrown.  Lastly, if the duration until the next time tokens will
-     * be added to the token bucket is less than a single unit of the passed in time unit then this method will
-     * return 0.
-     *
-     * @param unit The time unit to express the return value in.
-     * @return The amount of time until the next group of tokens can be added to the token bucket.
-     */
-    long getDurationUntilNextRefill(TimeUnit unit) throws UnsupportedOperationException;
-  }
-
-  /** Encapsulation of a strategy for relinquishing control of the CPU. */
-  static interface SleepStrategy
-  {
-    /**
-     * Sleep for a short period of time to allow other threads and system processes to execute.
-     */
-    void sleep();
-  }
+        scanner.close();
+    }
 }
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-
-
-/*
- * Copyright 2012-2014 Brandon Beck
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.isomorphism.util;
-
-import java.util.concurrent.TimeUnit;
-
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-
-/**
- * A token bucket implementation that is of a leaky bucket in the sense that it has a finite capacity and any added
- * tokens that would exceed this capacity will "overflow" out of the bucket and are lost forever.
- * <p/>
- * In this implementation the rules for refilling the bucket are encapsulated in a provided {@code RefillStrategy}
- * instance.  Prior to attempting to consume any tokens the refill strategy will be consulted to see how many tokens
- * should be added to the bucket.
- * <p/>
- * In addition in this implementation the method of yielding CPU control is encapsulated in the provided
- * {@code SleepStrategy} instance.  For high performance applications where tokens are being refilled incredibly quickly
- * and an accurate bucket implementation is required, it may be useful to never yield control of the CPU and to instead
- * busy wait.  This strategy allows the caller to make this decision for themselves instead of the library forcing a
- * decision.
- */
-class TokenBucketImpl implements TokenBucket
-{
-  private final long capacity;
-  private final RefillStrategy refillStrategy;
-  private final SleepStrategy sleepStrategy;
-  private long size;
-
-  TokenBucketImpl(long capacity, long initialTokens, RefillStrategy refillStrategy, SleepStrategy sleepStrategy)
-  {
-    checkArgument(capacity > 0);
-    checkArgument(initialTokens <= capacity);
-
-    this.capacity = capacity;
-    this.refillStrategy = checkNotNull(refillStrategy);
-    this.sleepStrategy = checkNotNull(sleepStrategy);
-    this.size = initialTokens;
-  }
-
-  /**
-   * Returns the capacity of this token bucket.  This is the maximum number of tokens that the bucket can hold at
-   * any one time.
-   *
-   * @return The capacity of the bucket.
-   */
-  @Override
-  public long getCapacity()
-  {
-    return capacity;
-  }
-
-  /**
-   * Returns the current number of tokens in the bucket.  If the bucket is empty then this method will return 0.
-   *
-   * @return The current number of tokens in the bucket.
-   */
-  @Override
-  public synchronized long getNumTokens()
-  {
-    // Give the refill strategy a chance to add tokens if it needs to so that we have an accurate
-    // count.
-    refill(refillStrategy.refill());
-
-    return size;
-  }
-
-  /**
-  * Returns the amount of time in the specified time unit until the next group of tokens can be added to the token
-  * bucket.
-  *
-  * @see org.isomorphism.util.TokenBucket.RefillStrategy#getDurationUntilNextRefill(java.util.concurrent.TimeUnit)
-  * @param unit The time unit to express the return value in.
-  * @return The amount of time until the next group of tokens can be added to the token bucket.
-  */
-  @Override
-  public long getDurationUntilNextRefill(TimeUnit unit) throws UnsupportedOperationException
-  {
-    return refillStrategy.getDurationUntilNextRefill(unit);
-  }
-
-  /**
-   * Attempt to consume a single token from the bucket.  If it was consumed then {@code true} is returned, otherwise
-   * {@code false} is returned.
-   *
-   * @return {@code true} if a token was consumed, {@code false} otherwise.
-   */
-  public boolean tryConsume()
-  {
-    return tryConsume(1);
-  }
-
-  /**
-   * Attempt to consume a specified number of tokens from the bucket.  If the tokens were consumed then {@code true}
-   * is returned, otherwise {@code false} is returned.
-   *
-   * @param numTokens The number of tokens to consume from the bucket, must be a positive number.
-   * @return {@code true} if the tokens were consumed, {@code false} otherwise.
-   */
-  public synchronized boolean tryConsume(long numTokens)
-  {
-    checkArgument(numTokens > 0, "Number of tokens to consume must be positive");
-    checkArgument(numTokens <= capacity, "Number of tokens to consume must be less than the capacity of the bucket.");
-
-    refill(refillStrategy.refill());
-
-    // Now try to consume some tokens
-    if (numTokens <= size) {
-      size -= numTokens;
-      return true;
-    }
-
-    return false;
-  }
-
-  /**
-   * Consume a single token from the bucket.  If no token is currently available then this method will block until a
-   * token becomes available.
-   */
-  public void consume()
-  {
-    consume(1);
-  }
-
-  /**
-   * Consumes multiple tokens from the bucket.  If enough tokens are not currently available then this method will block
-   * until
-   *
-   * @param numTokens The number of tokens to consume from teh bucket, must be a positive number.
-   */
-  public void consume(long numTokens)
-  {
-    while (true) {
-      if (tryConsume(numTokens)) {
-        break;
-      }
-
-      sleepStrategy.sleep();
-    }
-  }
-
-  /**
-   * Refills the bucket with the specified number of tokens.  If the bucket is currently full or near capacity then
-   * fewer than {@code numTokens} may be added.
-   *
-   * @param numTokens The number of tokens to add to the bucket.
-   */
-  public synchronized void refill(long numTokens)
-  {
-    long newTokens = Math.min(capacity, Math.max(0, numTokens));
-    size = Math.max(0, Math.min(size + newTokens, capacity));
-  }
-}
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-
-
-/*
- * Copyright 2012-2014 Brandon Beck
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.isomorphism.util;
-
-import com.google.common.base.Ticker;
-import com.google.common.util.concurrent.Uninterruptibles;
-
-import java.util.concurrent.TimeUnit;
-
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-
-/** Static utility methods pertaining to creating {@link TokenBucketImpl} instances. */
-public final class TokenBuckets
-{
-  private TokenBuckets() {}
-
-  /** Create a new builder for token buckets. */
-  public static Builder builder()
-  {
-    return new Builder();
-  }
-
-  public static class Builder
-  {
-    private Long capacity = null;
-    private long initialTokens = 0;
-    private TokenBucketImpl.RefillStrategy refillStrategy = null;
-    private TokenBucketImpl.SleepStrategy sleepStrategy = YIELDING_SLEEP_STRATEGY;
-    private final Ticker ticker = Ticker.systemTicker();
-
-    /** Specify the overall capacity of the token bucket. */
-    public Builder withCapacity(long numTokens)
-    {
-      checkArgument(numTokens > 0, "Must specify a positive number of tokens");
-      capacity = numTokens;
-      return this;
-    }
-
-    /** Initialize the token bucket with a specific number of tokens. */
-    public Builder withInitialTokens(long numTokens)
-    {
-      checkArgument(numTokens > 0, "Must specify a positive number of tokens");
-      initialTokens = numTokens;
-      return this;
-    }
-
-    /** Refill tokens at a fixed interval. */
-    public Builder withFixedIntervalRefillStrategy(long refillTokens, long period, TimeUnit unit)
-    {
-      return withRefillStrategy(new FixedIntervalRefillStrategy(ticker, refillTokens, period, unit));
-    }
-
-    /** Use a user defined refill strategy. */
-    public Builder withRefillStrategy(TokenBucket.RefillStrategy refillStrategy)
-    {
-      this.refillStrategy = checkNotNull(refillStrategy);
-      return this;
-    }
-
-    /** Use a sleep strategy that will always attempt to yield the CPU to other processes. */
-    public Builder withYieldingSleepStrategy()
-    {
-      return withSleepStrategy(YIELDING_SLEEP_STRATEGY);
-    }
-
-    /**
-     * Use a sleep strategy that will not yield the CPU to other processes.  It will busy wait until more tokens become
-     * available.
-     */
-    public Builder withBusyWaitSleepStrategy()
-    {
-      return withSleepStrategy(BUSY_WAIT_SLEEP_STRATEGY);
-    }
-
-    /** Use a user defined sleep strategy. */
-    public Builder withSleepStrategy(TokenBucket.SleepStrategy sleepStrategy)
-    {
-      this.sleepStrategy = checkNotNull(sleepStrategy);
-      return this;
-    }
-
-    /** Build the token bucket. */
-    public TokenBucket build()
-    {
-      checkNotNull(capacity, "Must specify a capacity");
-      checkNotNull(refillStrategy, "Must specify a refill strategy");
-
-      return new TokenBucketImpl(capacity, initialTokens, refillStrategy, sleepStrategy);
-    }
-  }
-
-  private static final TokenBucketImpl.SleepStrategy YIELDING_SLEEP_STRATEGY = new TokenBucketImpl.SleepStrategy()
-  {
-    @Override
-    public void sleep()
-    {
-      // Sleep for the smallest unit of time possible just to relinquish control
-      // and to allow other threads to run.
-      Uninterruptibles.sleepUninterruptibly(1, TimeUnit.NANOSECONDS);
-    }
-  };
-
-  private static final TokenBucketImpl.SleepStrategy BUSY_WAIT_SLEEP_STRATEGY = new TokenBucketImpl.SleepStrategy()
-  {
-    @Override
-    public void sleep()
-    {
-      // Do nothing, don't sleep.
-    }
-  };
-}
-
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
